9708127c44c61df2861f39e83c449201c10529b1
Aanya Chandarana <aanyakch@gmail.com>
2025-08-08 13:33:11 -0700

    Initial Commit

---
+++ ToDoListValCreated/ToDoListValCreatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListValCreatedApp.swift
+//  ToDoListValCreated
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListValCreatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
